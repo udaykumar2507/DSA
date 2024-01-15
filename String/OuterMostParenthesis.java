3c573498e7428de1c61d35f909720b81ed68dbf9
Uday Kumar Maurya <122080383+udaykumar2507@users.noreply.github.com>
2024-01-15 22:29:16 +0530

    Add files via upload

---
+++ String/OuterMostParenthesis.java
@@ -0,0 +1,25 @@
+package String;
+
+public class OuterMostParenthesis {
+        public String removeOuterParentheses(String s) {
+           StringBuilder ans = new StringBuilder();
+            int cnt = 0;
+    
+            for (int i = 0; i < s.length(); i++) {
+                char c = s.charAt(i);
+    
+                if (c == '(') {
+                    if (++cnt > 1) {
+                        ans.append(c);
+                    }
+                } else if (c == ')') {
+                    if (--cnt > 0) {
+                        ans.append(c);
+                    }
+                }
+            }
+            return ans.toString();
+    }
+    }
+    
+
