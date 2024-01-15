61c81d6eddd5da8ef98b7756093b5a015954a315
Uday Kumar Maurya <122080383+udaykumar2507@users.noreply.github.com>
2024-01-15 22:32:05 +0530

    Create Largest_Integer.java

---
+++ String/Largest_Integer.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String largestOddNumber(String nums) {
+        int i=nums.length()-1;
+        while(i>=0){
+            int num=Character.getNumericValue(nums.charAt(i));
+            if (num%2==1){
+                break;
+            }else{
+                i--;
+            }
+        }
+        if (i==-1){
+            return "";
+        }else{
+            return nums.substring(0,i+1); 
+        }
+
+        
+    }
+}
