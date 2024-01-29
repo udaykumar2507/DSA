2685ad9807789d4e8b11f002fd4379bc90b2674a
Uday Kumar Maurya <122080383+udaykumar2507@users.noreply.github.com>
2024-01-29 20:34:05 +0530

    Create Reverse_a_SLL_Iterative.java

---
+++ Linked_List/Medium_prob_of_LL/Reverse_a_SLL_Iterative.java
@@ -0,0 +1,13 @@
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode temp=head;
+        ListNode prev=null;
+        while(temp!=null){
+            ListNode front=temp.next;
+            temp.next=prev;
+            prev=temp;
+            temp=front;
+        }
+        return prev;
+    }
+}
